7c1c0325fedd7dad7525bf55ea71587248dbaeb6
chiemeliecode <ezeokeke.c@northeastern.edu>
2022-02-28 14:48:16 -0500

    a5 JavaScript sp22

---
+++ public/tuiter/Explorescreen/ExploreComponent.js
@@ -0,0 +1,45 @@
+import PostSummaryList from "../PostSummaryList";
+
+const ExploreComponent = () => {
+  return(`
+       <div class="col-7">
+            <div class="row">
+                       <!-- search field and cog -->
+                       <div class="form-group has-search">
+                          <span class="fa fa-search form-control-feedback"></span>
+                          <input type="text" class="form-control fa fa-cog rounded-pill " placeholder="Search Tuiter..."/>
+                          <span class="fa fa-cog fa-2x wd-color-cog "></span>
+                      </div>
+           </div>
+           <ul class="nav mb-2 nav-tabs">
+                      <!-- tabs -->
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">For You</a>
+              </li>
+              <li class="nav-item">
+                <a href="" href="trending.html" class="nav-link" href="#">Trending</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link" href="#">Covid-19</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">News</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Sport</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Entertainment</a>
+              </li>
+           </ul>
+           <!-- image with overlaid text -->
+           <div>
+           <img class="img-fluid rounded mx-auto" src="https://techcrunch.com/wp-content/uploads/2019/09/Starship-Mk1-Day.jpg" width="100%" />
+           <strong class="fs-3 wd-bottom-lefts wd-font-color">SpaceX's Starship</strong>
+           </div>
+       
+           ${PostSummaryList()}
+       </div>
+    `);
+}
+export default ExploreComponent;
