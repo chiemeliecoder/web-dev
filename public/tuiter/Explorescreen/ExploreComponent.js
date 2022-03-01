bc7c77d2f723dd22c84a29c78cbb178d42a0a97b
chiemeliecode <ezeokeke.c@northeastern.edu>
2022-03-01 18:53:14 -0500

    a5 JavaScript sp22

--- public/tuiter/Explorescreen/ExploreComponent.js
+++ public/tuiter/Explorescreen/ExploreComponent.js
@@ -9,7 +9,7 @@ const ExploreComponent = () => {
                           <span class="fa fa-search form-control-feedback"></span>
                           <input type="text" class="form-control rounded-pill " placeholder="Search Tuiter..."/>
                       </div>
-                      <div class="col-1"><i class="fa fa-cog fa-2x wd-color-cog "></i></div>
+                      <div class="col-1"><i class="fa fa-cog fa-2x wd-color-cog wd-side-cog "></i></div>
            </div>
            <ul class="nav mb-2 nav-tabs">
                       <!-- tabs -->
