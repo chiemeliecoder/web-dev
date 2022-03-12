d2bf61f1fd690d1f9c5f08d9557f40cef3eb6481
chiemeliecode <ezeokeke.c@northeastern.edu>
2022-03-12 11:40:44 -0500

    a6 React.js sp22

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './explore.css';
+import './vendors/fontawesome/css/all.min.css';
+import HelloWorld from "./components/HelloWorld";
+import Index from "./components/Labs";
+import Tuiter from "./components/Tuiter/Tuiter";
+import ExploreScreen from "./components/Tuiter/Explorescreen/ExploreScreen";
+import HomeScreen from "./components/Tuiter/HomeScreen/HomeScreen"
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/hello" exact element={<HelloWorld />} />
+            <Route path= "/labs" exact element={<Index />} />
+            <Route path= "/" exact element={<Index/>}/>
+            <Route path="/tuiter" exact element={<Tuiter />}/>
+            <Route path = "/tuiter/explore" exact element={<ExploreScreen/>}/>
+              <Route path = "/tuiter/home" exact element={<HomeScreen/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
   );
 }
 
---
+++ src/components/Labs/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoList from "./Todo/TodoList";
+import {Link} from "react-router-dom";
+
+const Index = () => {
+  return (
+      <>
+        <h1>Labs!</h1>
+        <Link to="/hello">Hello</Link>|
+        <Link to="/tuiter">Tuiter</Link>
+        <TodoList/>
+        <ConditionalOutput/>
+        <Styles/>
+        <Classes/>
+      </>
+  )
+};
+
+export default Index;
